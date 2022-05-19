c99fc3ddddd8913339ed74ed476bf5bce981d698
Aditya Mishra <mishraaaditya29@gmail.com>
2022-05-19 11:45:53 +0530

    git commit

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { data } from '../../../data';
+import { data } from '../../../data';// importing data array
 
 const UseStateArray = () => {
+  const [people, setPeople] = React.useState(data);// Setting default array
   return ( 
     <>
-    
+      {people.map((person) =>{// mapping over array of people
+        const {id, name} = person;//destructuring data array in person
+        return (
+          <div key={id} className='item'>
+            <h4>{name}</h4>
+          </div>
+        )
+      })}
     </>
   )
 };
