ff7a5fb32a3dc0fb66c1b0fd46439cbfce2287c5
Aditya Mishra <mishraaaditya29@gmail.com>
2022-05-19 12:14:14 +0530

    git commit

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,6 +3,13 @@ import { data } from '../../../data';// importing data array
 
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);// Setting default array
+
+  // setting function to remove an individual item from id
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id)//filtering out people whose id isn't matched when onClick is invoked so that they remain on screen and returned to new people and the id clicked is deleted
+    setPeople(newPeople)//passing newPeople in setPeople
+  }
+
   return ( 
     <>
       {people.map((person) =>{// mapping over array of people
@@ -10,9 +17,13 @@ const UseStateArray = () => {
         return (
           <div key={id} className='item'>
             <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>Remove</button>  
           </div>
         )
       })}
+      <button className='btn' onClick={() => setPeople([])}>
+        clear items
+      </button>
     </>
   )
 };
